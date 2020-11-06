99e376fccf8b82c971f6001fd08fc48b9bb4ff6a
Sensweii <alen.dg.balbuena@gmail.com>
2020-11-06 20:25:53 +0800

    Removed login action

--- users/api/v1/views.py
+++ users/api/v1/views.py
@@ -1,15 +1,10 @@
-from django.contrib.auth import authenticate
-from rest_framework import status
 from rest_framework import viewsets
-from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 
+from api.v1.authentications import OAuthHandler
 from users.models import User
-from .authentications import OAuthHandler
 from .permissions import UserUpdatePermission
 from .serializers import UserCreateSerializer
-from .serializers import UserLoginSerializer
 from .serializers import UserPartialSerializer
 from .serializers import UserSerializer
 from .utils import EmailSender
@@ -46,24 +41,4 @@ class UserViewSet(viewsets.ModelViewSet):
         user = self.get_object()
         password = self.request.data.get('password')
         user.update_password(password=password)
-
-    @action(detail=False, methods=['post'])
-    def login(self, request):
-        serializer = UserLoginSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST)
-        user = authenticate(
-            email=serializer.validated_data['email'],
-            password=serializer.validated_data['password'])
-        if not user:
-            return Response(
-            {'Invalid credentials.'},
-            status=status.HTTP_401_UNAUTHORIZED)
-
-        # Generate token for logged in user
-        token = OAuthHandler.create_token(user)
-        return Response(
-            token,
-            status=status.HTTP_200_OK)
+        OAuthHandler.create_app(user)
