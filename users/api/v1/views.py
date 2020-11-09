0a33f385944c8fbf1ba45f753c6320f4c000548e
Sensweii <alen.dg.balbuena@gmail.com>
2020-11-09 20:58:33 +0800

    Adjust status action for handling user activation.

--- users/api/v1/views.py
+++ users/api/v1/views.py
@@ -1,8 +1,13 @@
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from users.models import User
+from .permissions import UserActivatePermission
 from .permissions import UserUpdatePermission
+from .serializers import UserActivateSerializer
 from .serializers import UserCreateUpdateSerializer
 from .serializers import UserPartialSerializer
 from .serializers import UserSerializer
@@ -20,6 +25,8 @@ class UserViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action in ['partial_update', 'update', 'destroy']:
             return [UserUpdatePermission()]
+        elif self.action in ['status']:
+            return [UserActivatePermission()]
         return super().get_permissions()
 
     def get_serializer_class(self):
@@ -44,3 +51,23 @@ class UserViewSet(viewsets.ModelViewSet):
         password = self.request.data.get('password')
         if password:
             user.update_password(password)
+
+    @action(detail=True, methods=['patch'])
+    def status(self, request, *args, **kwargs):
+        """
+        Method for user activation. Needs proper authorization header to update
+        user `is_activated` field.
+        """
+        instance = self.get_object()
+        serializer = UserActivateSerializer(
+            instance,
+            data=request.data,
+            partial=True,
+            context={'request': request})
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
