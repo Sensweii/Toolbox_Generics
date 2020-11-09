b49703f7eac841a42bd6e42df48a21a90d6414d3
Sensweii <alen.dg.balbuena@gmail.com>
2020-11-09 20:59:25 +0800

    Refactor permissions to separate user update from user activation.

--- users/api/v1/permissions.py
+++ users/api/v1/permissions.py
@@ -1,7 +1,8 @@
+import jwt
+
 from django.conf import settings
 from django.utils import timezone
 
-import jwt
 from oauth2_provider.models import AccessToken
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import BasePermission
@@ -11,12 +12,30 @@ from users.models import User
 
 class UserUpdatePermission(BasePermission):
     """
-        Custom permission for handling PATCH requests to a user resource.
-        There are two cases to handle:
-            1. Updating `is_activated` field (Activation) will require a
-                json webtoken (activation code sent by email).
-            2. Updating user's info (e.g. email, name, password, etc.) will
-                require login (oauth token authorization).
+    Permission class for handling PATCH requests to a user resource.
+    """
+
+    def verify_oauth_token(self, request, obj):
+        token = request.headers.get('Authorization')
+        if not token:
+            raise PermissionDenied(detail='Restricted access.')
+        try:
+            formatted_token = token.replace('Bearer ', '')
+            AccessToken.objects.get(
+                user=obj,
+                token=formatted_token,
+                expires__gt=timezone.now())
+        except AccessToken.DoesNotExist:
+            raise PermissionDenied(detail='Invalid token.')
+        return True
+
+    def has_object_permission(self, request, view, obj):        
+        return self.verify_oauth_token(request, obj)
+
+
+class UserActivatePermission(BasePermission):
+    """
+    Permission class for handling PATCH requests to user's is_activated field.
     """
 
     def verify_activation_token(self, request, obj):
@@ -28,8 +47,7 @@ class UserUpdatePermission(BasePermission):
         except jwt.exceptions.InvalidSignatureError:
             raise PermissionDenied(detail='Token signature mismatch.')
         except jwt.exceptions.DecodeError:
-            raise PermissionDenied(
-                detail='Invalid token.')
+            raise PermissionDenied(detail='Invalid token.')
         except TypeError:
             raise PermissionDenied('Invalid token.')
         if decoded_token['recipient'] != user_id:
@@ -38,30 +56,5 @@ class UserUpdatePermission(BasePermission):
             return True
         return False
 
-    def verify_oauth_token(self, request, obj):
-        token = request.headers.get('Authorization')
-        if not token:
-            raise PermissionDenied(detail='Restricted access.')
-        try:
-            formatted_token = token.replace('Bearer ', '')
-            access = AccessToken.objects.get(
-                user=obj,
-                token=formatted_token,
-                expires__gt=timezone.now())
-        except AccessToken.DoesNotExist:
-            raise PermissionDenied(detail='Invalid token.')
-        return True
-
-
-    def has_object_permission(self, request, view, obj):
-        # Check if request is activation or info update
-        activation_request = request.data.get('is_activated')
-        if activation_request:
-            permission_granted = self.verify_activation_token(request, obj)
-        elif type(activation_request) == bool:
-            raise PermissionDenied(detail=f'Reversing activation not allowed. '
-                f'Delete the account instead.')
-            permission_granted = False
-        else:
-            permission_granted = self.verify_oauth_token(request, obj)        
-        return permission_granted
+    def has_object_permission(self, request, view, obj):        
+        return self.verify_activation_token(request, obj)
