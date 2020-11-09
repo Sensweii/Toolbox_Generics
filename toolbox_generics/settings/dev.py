f34a3f2535071c7e6f8778b65c532754bf7a2d7e
Sensweii <alen.dg.balbuena@gmail.com>
2020-11-09 22:53:07 +0800

    Bug fixed: url conf not properly imported.

--- toolbox_generics/settings/dev.py
+++ toolbox_generics/settings/dev.py
@@ -1,5 +1,11 @@
 from .base import *
 
 
-BASE_URL = 'http://localhost:8000'
 DEBUG = True
+
+# Url conf
+BASE_URL = 'http://localhost:8000'
+API_USERS_URL = BASE_URL + '/api/v1/users/'
+
+# OAuth
+OAUTH_TOKEN_ENDPOINT = BASE_URL + '/token/'
